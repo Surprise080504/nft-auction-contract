857739b53ce0d527d64055d2fee774b12af2a1a0
superninja0119 <superninja0119@gmail.com>
2022-04-06 11:14:28 +0900

    complete nft auction contract

---
+++ hardhat.config.js
@@ -0,0 +1,63 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-ethers");
+const {
+  rinkebyPrivateKey,
+  alchemyUrl,
+  etherscanApiKey,
+  coinmarketCapKey,
+} = require("./secretsManager.example.js");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const RINKEBY_PRIVATE_KEY = rinkebyPrivateKey;
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 500,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    gasPrice: 30,
+    coinmarketcap: coinmarketCapKey,
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+
+  //uncomment this and run: yarn deploy-rinkeby
+  // networks: {
+  //   rinkeby: {
+  //     url: alchemyUrl,
+  //     accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+  //   },
+  // },
+  // etherscan: {
+  //   // Your API key for Etherscan
+  //   // Obtain one at https://etherscan.io/
+  //   apiKey: etherscanApiKey,
+  // },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const ERC721 = await ethers.getContractFactory("ERC721MockContract");
+  const ERC20 = await ethers.getContractFactory("ERC20MockContract");
+  const NFTAuction = await ethers.getContractFactory("NFTAuction");
+  const nftAuction = await NFTAuction.deploy();
+  const erc20 = await ERC20.deploy("Mock ERC20", "MER");
+
+  const erc721 = await ERC721.deploy("New Mocks", "NMK");
+
+  //verify: npx hardhat verify --network rinkeby DEPLOYED_CONTRACT_ADDRESS
+  console.log("nftAuction address:", nftAuction.address);
+  //verify: npx hardhat verify --network rinkeby --constructor-args erc20Arguments.js DEPLOYED_CONTRACT_ADDRESS
+  console.log("ERC20 Token address:", erc20.address);
+  // //verify: npx hardhat verify --network rinkeby --constructor-args arguments.js DEPLOYED_CONTRACT_ADDRESS
+  console.log("ERC721 Token address:", erc721.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
